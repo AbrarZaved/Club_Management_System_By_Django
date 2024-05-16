8d3b1453f1be467aadd73b056d7297f025d246a0
AbrarZaved <abrarzaved2002@gmail.com>
2024-05-16 12:15:35 +0600

    Refactor URL paths and update user template

--- DIU_CLUBS/urls.py
+++ DIU_CLUBS/urls.py
@@ -4,8 +4,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('dash/',include("Dashboard.urls")),
-    path('auth/',include("authentication.urls"))
+    path('admin', admin.site.urls),
+    path('dashboard/',include("Dashboard.urls")),
+    path('',include("authentication.urls"))
     
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
